cf90e1c998136ab75f5a0a4dcc59e1a301129680
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-11-22 17:13:13 -0500

    Add Edit and Submit interactivity to Experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,52 +2,92 @@ import React, { Component } from "react";
 import "../styles/experience.scss";
 
 class Experience extends Component {
-  render() {
-    const experienceInfo = {
-      companyName: "GOOGLE",
-      title: "Software Engineer",
-      date: "2019",
-      description: "AWDAWDawdawdwadAWDawD",
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: "",
+      title: "",
+      date: "",
+      description: "",
+      editMode: false,
     };
-    const editMode = true;
+  }
+
+  handleInputChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleEdit = (event) => {
+    event.preventDefault();
+    const { editMode } = this.state;
+    this.setState({
+      editMode: !editMode,
+    });
+  };
+
+  render() {
+    const { company, title, date, description } = this.state;
+    const { editMode } = this.state;
     if (editMode) {
       return (
-        <div className="experience-form-block">
-          <form className="experience-form">
-            <input
-              type="text"
-              name="company"
-              id="company"
-              placeholder="Company"
-            />
-            <input type="text" name="title" id="title" placeholder="Title" />
-            <input type="date" name="date" id="date" placeholder="Date" />
-            <textarea
-              name="description"
-              id="description"
-              placeholder="Describe your work"
-              rows="5"
-            />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+        <form className="experience-form">
+          <input
+            type="text"
+            name="company"
+            id="company"
+            placeholder="Company"
+            value={company}
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Title"
+            value={title}
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="date"
+            name="date"
+            id="date"
+            placeholder="Date"
+            value={date}
+            onChange={this.handleInputChange}
+          />
+          <textarea
+            name="description"
+            id="description"
+            rows="5"
+            placeholder="Describe your work"
+            value={description}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit" onClick={this.handleEdit}>
+            Submit
+          </button>
+        </form>
       );
     }
     return (
       <div className="experience-block">
         <div className="experience-data">
-          <p>Company: {experienceInfo.companyName}</p>
-          <p>Title: {experienceInfo.title}</p>
-          <p>Date: {experienceInfo.date}</p>
-          <p>Description: {experienceInfo.description}</p>
+          <p>Company: {company}</p>
+          <p>Title: {title}</p>
+          <p>Date: {date}</p>
+          <p>Description: {description}</p>
         </div>
         <div className="view-experience-buttons">
-          <button type="button" id="edit-experience-button">
+          <button type="button" onClick={this.handleEdit}>
             edit
           </button>
-          <button type="button" id="delete-experience-button">
-            delete
-          </button>
+          <button type="button">delete</button>
         </div>
       </div>
     );
