bfbe917a4e60fd6f5a64c65ec69fffef5bf34dcf
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-11-21 23:21:11 -0500

    Add Edit and Submit interactivity to Education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,29 +2,64 @@ import React, { Component } from "react";
 import "../styles/education.scss";
 
 class Education extends Component {
-  render() {
-    const educationInfo = {
-      schoolName: "CNU",
-      degree: "Computer Engineering",
-      graduationYear: "2019",
+  constructor(props) {
+    super(props);
+    this.state = {
+      school: "",
+      degree: "",
+      graduation: "",
+      editMode: true,
     };
-    const editMode = false;
+  }
+
+  handleInputChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    const { name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleEdit = () => {
+    const { editMode } = this.state;
+    this.setState({
+      editMode: !editMode,
+    });
+  };
+
+  render() {
+    const { editMode } = this.state;
+    const { school, degree, graduation } = this.state;
+
     if (editMode) {
       return (
         <div className="education-form-block">
-          <form className="education-form">
+          <form className="education-form" onSubmit={this.handleEdit}>
             <input
               type="text"
               name="school"
               id="school"
               placeholder="School Name"
+              value={school}
+              onChange={this.handleInputChange}
             />
-            <input type="text" name="degree" id="degree" placeholder="Degree" />
+            <input
+              type="text"
+              name="degree"
+              id="degree"
+              placeholder="Degree"
+              value={degree}
+              onChange={this.handleInputChange}
+            />
             <input
               type="text"
               name="graduation"
               id="graduation"
               placeholder="Graduation Year"
+              value={graduation}
+              onChange={this.handleInputChange}
             />
             <button type="submit">Submit</button>
           </form>
@@ -34,12 +69,16 @@ class Education extends Component {
     return (
       <div className="education-block">
         <div className="education-data">
-          <p>School: {educationInfo.schoolName}</p>
-          <p>Degree: {educationInfo.degree}</p>
-          <p>Graduation Year: {educationInfo.graduationYear}</p>
+          <p>School: {school}</p>
+          <p>Degree: {degree}</p>
+          <p>Graduation Year: {graduation}</p>
         </div>
         <div className="view-education-buttons">
-          <button type="button" id="edit-education-button">
+          <button
+            type="button"
+            id="edit-education-button"
+            onClick={this.handleEdit}
+          >
             edit
           </button>
           <button type="button" id="delete-education-button">
