643d37e74a02b316ab02736cc8ecead1826c7190
De'Shawn <deshawn.nathaniel.maynard@gmail.com>
2022-11-21 21:34:30 -0500

    Refactor Info Component - combined two sub components back into one main component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import EditInfo from "./EditInfo";
-import ViewInfo from "./ViewInfo";
 import "../styles/info.scss";
 
 class Info extends Component {
@@ -28,15 +26,69 @@ class Info extends Component {
   render() {
     const { editMode } = this.state;
     const { first, last, email, phone } = this.state;
+
     if (editMode) {
       return (
-        <EditInfo
-          info={{ first, last, email, phone }}
-          handleInputChange={this.handleInputChange}
-        />
+        <form id="info-form">
+          <input
+            type="text"
+            id="first"
+            name="first"
+            placeholder="First Name"
+            value={first}
+            onChange={this.handleInputChange}
+          />
+
+          <input
+            type="text"
+            id="last"
+            name="last"
+            placeholder="Last Name"
+            value={last}
+            onChange={this.handleInputChange}
+          />
+
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email@email.com"
+            value={email}
+            onChange={this.handleInputChange}
+          />
+
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="Phone 000-000-0000"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            value={phone}
+            onChange={this.handleInputChange}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
       );
     }
-    return <ViewInfo info={{ first, last, email, phone }} />;
+    return (
+      <div className="info">
+        <div className="info-data">
+          <p>First Name: {first}</p>
+          <p>Last Name: {last}</p>
+          <p>Email: {email}</p>
+          <p>Phone: {phone}</p>
+        </div>
+        <div className="info-buttons">
+          <button type="button" id="edit-info-button">
+            edit
+          </button>
+          <button type="button" id="delete-info-button">
+            delete
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
